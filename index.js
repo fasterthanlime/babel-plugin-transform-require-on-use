baac379518f14aec2a31772c2e13b26e096a09e6
Amos Wenger <amos@itch.io>
2017-07-27 21:51:14 +0200

    :arrow_up: 2.0.0

--- index.js
+++ index.js
@@ -25,21 +25,48 @@ module.exports = function({ types: t }) {
           return;
         }
 
-        const binding = path.scope.bindings[parent.node.id.name];
+        const parentId = parent.get("id");  
+        if (!parentId.isIdentifier()) {
+          // could be something like `const {a, b, c} = require(something);`
+          // let's leave it alone
+          return;
+        }
+
+        const importName = parentId.node.name;
+
+        const binding = path.scope.getBinding(importName);
+        if (!binding) {
+          // well, if it's not bound, there's probably side effects
+          // and also no point in delaying it
+          return;
+        }
+
         for (const refPath of binding.referencePaths) {
           refPath.replaceWith(t.callExpression(refPath.node, []));
         }
 
+        const cachedModuleId = path.scope.generateUidIdentifier("get" + importName);
+        path.scope.push(t.variableDeclarator(cachedModuleId, t.nullLiteral()));
         path.replaceWith(t.functionExpression(
           // id
-          parent.node.id,
+          parentId.node,
 
           // params
           [],
 
           // body
           t.blockStatement([
-            t.returnStatement(path.node)
+            t.returnStatement(
+              t.conditionalExpression(
+                cachedModuleId,
+                cachedModuleId,
+                t.assignmentExpression(
+                  "=",
+                  cachedModuleId,
+                  path.node
+                )
+              )
+            )
           ]),
          ));
       },
